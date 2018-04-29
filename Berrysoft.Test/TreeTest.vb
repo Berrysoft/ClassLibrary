b84d8dd0fbe57191f9cc162052eba8a41a2b23c5
王宇逸 <Strawberry_Str@hotmail.com>
2018-04-29 22:46:38 +0800

    Add an unit test.

---
+++ Berrysoft.Test/TreeTest.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text
+Imports Berrysoft.Data
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()>
+Public Class TreeTest
+
+    <TestMethod()>
+    Public Sub DepthTest()
+        Dim tree As New Tree(Of Integer)(1)
+        Dim node2 As New Node(Of Integer)(2)
+        Dim node3 As New Node(Of Integer)(3)
+        Dim node4 As New Node(Of Integer)(4)
+        tree.Root.Add(node2)
+        node2.Add(node3)
+        node2.Add(node4)
+        Assert.AreEqual(tree.GetDepth(), 3)
+    End Sub
+
+End Class
