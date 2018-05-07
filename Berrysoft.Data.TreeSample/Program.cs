916b781df1526770cf7f1823fef6b8a06d708812
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-07 20:47:16 +0800

    Change platform to .net core, change sample from vb to c#.

---
+++ Berrysoft.Data.TreeSample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Berrysoft.Data.TreeSample
+{
+    class Program
+    {
+        static BinaryTree<int> tree;
+        static BinaryNode<int> current;
+        static void Main(string[] args)
+        {
+            tree = new BinaryTree<int>();
+            Console.WriteLine("How many groups do you want to test: ");
+            int n = int.Parse(Console.ReadLine());
+            while (n-- > 0)
+            {
+                Console.WriteLine("Please enter the values by pre order, splited by space:");
+                int[] front = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
+                Console.WriteLine("Please enter the values by in order, splited by space:");
+                int[] mid = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
+                if (front.Length != mid.Length)
+                {
+                    Console.WriteLine("The length of the two arrays should be equal.");
+                    continue;
+                }
+                tree.Root.LeftChild = null;
+                tree.Root.RightChild = null;
+                current = tree.Root;
+                Create(front, mid);
+                Console.WriteLine("The post order of this tree is:");
+                Console.WriteLine(String.Join(" ", tree.AsPostOrderEnumerable().Select(node => node.ToString()).ToArray()));
+            }
+        }
+        static void Create(in Span<int> front, in Span<int> mid)
+        {
+            int n = front.Length;
+            BinaryNode<int> tr = current;
+            tr.Value = front[0];
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                if (mid[i] == front[0])
+                {
+                    break;
+                }
+            }
+            if (i > 0)
+            {
+                current = (tr.LeftChild = new BinaryNode<int>());
+                Create(front.Slice(1, i), mid);
+            }
+            if (n - 1 - i > 0)
+            {
+                current = (tr.RightChild = new BinaryNode<int>());
+                Create(front.Slice(i + 1, n - 1 - i), mid.Slice(i + 1, n - 1 - i));
+            }
+        }
+    }
+}
