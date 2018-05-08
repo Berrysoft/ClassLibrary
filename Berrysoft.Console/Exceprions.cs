0579cba027fe1b66e5106a54385dcb1f711ded56
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-08 15:05:04 +0800

    Add CommandLine.

---
+++ Berrysoft.Console/Exceprions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Berrysoft.Console
+{
+    public class ArgNotValidException : Exception
+    {
+        public ArgNotValidException()
+            : this(null, null, null)
+        { }
+        public ArgNotValidException(string argName)
+            : this(argName, null, null)
+        { }
+        public ArgNotValidException(string argName, string message)
+            : this(argName, message, null)
+        { }
+        public ArgNotValidException(string argName, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            ArgName = argName;
+        }
+        public string ArgName { get; }
+    }
+    public class ArgRepeatedException : ArgNotValidException
+    {
+        public ArgRepeatedException()
+            : base()
+        { }
+        public ArgRepeatedException(string argName)
+            : base(argName, "Argument repeated.")
+        { }
+        public ArgRepeatedException(string argName, Exception innerException)
+            : base(argName, "Argument repeated.", innerException)
+        { }
+    }
+    public class ArgRequiredException : ArgNotValidException
+    {
+        public ArgRequiredException()
+            : base()
+        { }
+        public ArgRequiredException(string argName)
+            : base(argName, "Argument required.")
+        { }
+        public ArgRequiredException(string argName, Exception innerException)
+            : base(argName, "Argument requited.", innerException)
+        { }
+    }
+}
