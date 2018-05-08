af090d1be72f72d7c491edc3ec77ce4e3e7324fc
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-08 12:56:38 +0800

    Add Berrysoft.Console, not complited.

---
+++ Berrysoft.Console/CommandLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Berrysoft.Console
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
+    public sealed class OptionAttribute : Attribute
+    {
+        public OptionAttribute(char? shortArg, string longArg)
+        {
+            ShortArg = shortArg;
+            LongArg = longArg;
+        }
+        public char? ShortArg { get; }
+        public string LongArg { get; }
+        public bool Required { get; set; }
+        public string DefaultValue { get; set; }
+        public string HelpText { get; set; }
+    }
+    public abstract class CommandLine
+    {
+    }
+}
