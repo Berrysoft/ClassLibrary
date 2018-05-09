f5d24e96446a09a0d19ccb028da47093dec6b2d4
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-09 12:16:49 +0800

    Add Unsafe.

---
+++ Berrysoft.Unsafe/Pointer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Runtime.CompilerServices.Unsafe;
+
+namespace Berrysoft.Unsafe
+{
+    public unsafe struct Pointer<T>
+        where T : unmanaged
+    {
+        private T* _ptr;
+        public Pointer(T* ptr)
+        {
+            _ptr = ptr;
+        }
+        public Pointer(ref T ptr)
+        {
+            _ptr = (T*)AsPointer(ref ptr);
+        }
+        public T* Ptr => _ptr;
+        public IntPtr Handle => (IntPtr)_ptr;
+        public ref T Target => ref _ptr[0];
+        public ref T this[int index] => ref _ptr[index];
+    }
+}
