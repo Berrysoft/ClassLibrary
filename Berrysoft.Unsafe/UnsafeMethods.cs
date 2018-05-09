d20f2f178087c2bb6a091ebdb0c6766cbb432321
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-09 16:50:44 +0800

    Fix: `unmanaged` not avaliable in VB.

--- Berrysoft.Unsafe/UnsafeMethods.cs
+++ Berrysoft.Unsafe/UnsafeMethods.cs
@@ -1,37 +1,35 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using static System.Runtime.CompilerServices.Unsafe;
 
 namespace Berrysoft.Unsafe
 {
     public unsafe static class UnsafeMethods
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetSize<T>()
         {
             return SizeOf<T>();
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Pointer<T> AddressOf<T>(ref T value)
-            where T : unmanaged
         {
             return new Pointer<T>(ref value);
         }
-        public static Pointer<byte> AddressOfByte(ref byte value) => AddressOf(ref value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T TargetOf<T>(Pointer<T> ptr)
-            where T : unmanaged
         {
             return ref ptr.Target;
         }
-        public static ref byte TargetOfBytePtr(Pointer<byte> ptr) => ref TargetOf(ptr);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void StackAlloc<T>(int size, Action<Pointer<T>> action)
-            where T : unmanaged
         {
-            T* ptr = stackalloc T[size];
+            byte* ptr = stackalloc byte[size * GetSize<T>()];
             action(new Pointer<T>(ptr));
         }
-        public static void StackAllocSByte(int size, Action<Pointer<sbyte>> action) => StackAlloc(size, action);
-        public static void StackAllocByte(int size, Action<Pointer<byte>> action) => StackAlloc(size, action);
-        public static void StackAllocInt16(int size, Action<Pointer<short>> action) => StackAlloc(size, action);
-        public static void StackAllocUInt16(int size, Action<Pointer<ushort>> action) => StackAlloc(size, action);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MemorySet(IntPtr startAddress, byte value, uint byteCount) => InitBlock((void*)startAddress, value, byteCount);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MemoryCopy(IntPtr destination, IntPtr source, uint byteCount) => CopyBlock((void*)destination, (void*)source, byteCount);
     }
 }
