18abe8185e9d7184b87b7d46a9bb98b43d89e29e
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-11 21:00:10 +0800

    Move As*Enumerable to Enumerable.

--- Berrysoft.Data/Enumerable.cs
+++ Berrysoft.Data/Enumerable.cs
@@ -9,13 +9,7 @@ namespace Berrysoft.Data
         #region Tree
         public static IEnumerable<TNode> AsDFSEnumerable<TValue, TNode>(this ITree<TValue, TNode> tree)
             where TNode : INodeBase<TValue, TNode>
-        {
-            if (tree == null)
-            {
-                throw new ArgumentNullException(nameof(tree));
-            }
-            return AsDFSEnumerableIterator(tree);
-        }
+            => AsDFSEnumerableIterator(tree ?? throw new ArgumentNullException(nameof(tree)));
         private static IEnumerable<TNode> AsDFSEnumerableIterator<TValue, TNode>(ITree<TValue, TNode> tree)
             where TNode : INodeBase<TValue, TNode>
         {
@@ -33,13 +27,7 @@ namespace Berrysoft.Data
         }
         public static IEnumerable<TNode> AsBFSEnumerable<TValue, TNode>(this ITree<TValue, TNode> tree)
             where TNode : INodeBase<TValue, TNode>
-        {
-            if (tree == null)
-            {
-                throw new ArgumentNullException(nameof(tree));
-            }
-            return AsBFSEnumerableIterator(tree);
-        }
+            => AsBFSEnumerableIterator(tree ?? throw new ArgumentNullException(nameof(tree)));
         private static IEnumerable<TNode> AsBFSEnumerableIterator<TValue, TNode>(ITree<TValue, TNode> tree)
             where TNode : INodeBase<TValue, TNode>
         {
@@ -54,6 +42,202 @@ namespace Berrysoft.Data
                     nodes.Enqueue(child);
                 }
             }
+        }
+        #endregion
+        #region Graph
+        public static IEnumerable<T> AsDFSEnumerable<T>(this IGraph<T> graph, T root)
+        {
+            if (graph == null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+            if (!graph.Contains(root))
+            {
+                throw new KeyNotFoundException();
+            }
+            return AsDFSEnumerableIterator(graph, root);
+        }
+        private static IEnumerable<T> AsDFSEnumerableIterator<T>(IGraph<T> graph, T root)
+        {
+            Stack<T> nodes = new Stack<T>();
+            HashSet<T> visited = new HashSet<T>();
+            nodes.Push(root);
+            while (nodes.Count != 0)
+            {
+                T current;
+                do
+                {
+                    if (nodes.Count == 0)
+                    {
+                        yield break;
+                    }
+                    current = nodes.Pop();
+                }
+                while (visited.Contains(current));
+                visited.Add(current);
+                yield return current;
+                if (graph.TryGetHeads(current, out var heads))
+                {
+                    foreach (var child in heads.Reverse())
+                    {
+                        if (!visited.Contains(child))
+                        {
+                            nodes.Push(child);
+                        }
+                    }
+                }
+            }
+        }
+        public static IEnumerable<T> AsBFSEnumerable<T>(this IGraph<T> graph, T root)
+        {
+            if (graph == null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+            if (!graph.Contains(root))
+            {
+                throw new KeyNotFoundException();
+            }
+            return AsBFSEnumerableIterator(graph, root);
+        }
+        private static IEnumerable<T> AsBFSEnumerableIterator<T>(IGraph<T> graph, T root)
+        {
+            Queue<T> nodes = new Queue<T>();
+            HashSet<T> visited = new HashSet<T>();
+            nodes.Enqueue(root);
+            while (nodes.Count != 0)
+            {
+                T current;
+                do
+                {
+                    if (nodes.Count == 0)
+                    {
+                        yield break;
+                    }
+                    current = nodes.Dequeue();
+                }
+                while (visited.Contains(current));
+                visited.Add(current);
+                yield return current;
+                if (graph.TryGetHeads(current, out var heads))
+                {
+                    foreach (var child in heads)
+                    {
+                        if (!visited.Contains(child))
+                        {
+                            nodes.Enqueue(child);
+                        }
+                    }
+                }
+            }
+        }
+        public static Tree<T> ToDFSTree<T>(this Graph<T> graph, T root)
+            => ToDFSTree<T, Tree<T>, Node<T>>(graph, root);
+        public static TTree ToDFSTree<TValue, TTree, TNode>(this IGraph<TValue> graph, TValue root)
+            where TTree : ITree<TValue, TNode>, new()
+            where TNode : INode<TValue, TNode>, new()
+        {
+            if (graph == null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+            if (!graph.Contains(root))
+            {
+                throw new KeyNotFoundException();
+            }
+            TTree result = new TTree();
+            result.Root.Value = root;
+            Stack<TNode> nodes = new Stack<TNode>();
+            HashSet<TValue> visited = new HashSet<TValue>();
+            nodes.Push(result.Root);
+            while (nodes.Count != 0)
+            {
+                TNode current;
+                for (; ; )
+                {
+                    if (nodes.Count == 0)
+                    {
+                        goto ret;
+                    }
+                    current = nodes.Pop();
+                    if (visited.Contains(current.Value))
+                    {
+                        current.Parent.Remove(current);
+                        continue;
+                    }
+                    break;
+                }
+                visited.Add(current.Value);
+                if (graph.TryGetHeads(current.Value, out var heads))
+                {
+                    foreach (var child in heads.Reverse())
+                    {
+                        if (!visited.Contains(child))
+                        {
+                            TNode nc = new TNode();
+                            nc.Value = child;
+                            nodes.Push(nc);
+                            current.Add(nc);
+                        }
+                    }
+                }
+            }
+            ret:
+            return result;
+        }
+        public static Tree<T> ToBFSTree<T>(this Graph<T> graph, T root)
+            => ToBFSTree<T, Tree<T>, Node<T>>(graph, root);
+        public static TTree ToBFSTree<TValue, TTree, TNode>(this IGraph<TValue> graph, TValue root)
+            where TTree : ITree<TValue, TNode>, new()
+            where TNode : INode<TValue, TNode>, new()
+        {
+            if (graph == null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+            if (!graph.Contains(root))
+            {
+                throw new KeyNotFoundException();
+            }
+            TTree result = new TTree();
+            result.Root.Value = root;
+            Queue<TNode> nodes = new Queue<TNode>();
+            HashSet<TValue> visited = new HashSet<TValue>();
+            nodes.Enqueue(result.Root);
+            while (nodes.Count != 0)
+            {
+                TNode current;
+                for (; ; )
+                {
+                    if (nodes.Count == 0)
+                    {
+                        goto ret;
+                    }
+                    current = nodes.Dequeue();
+                    if (visited.Contains(current.Value))
+                    {
+                        current.Parent.Remove(current);
+                        continue;
+                    }
+                    break;
+                }
+                visited.Add(current.Value);
+                if (graph.TryGetHeads(current.Value, out var heads))
+                {
+                    foreach (var child in heads)
+                    {
+                        if (!visited.Contains(child))
+                        {
+                            TNode nc = new TNode();
+                            nc.Value = child;
+                            nodes.Enqueue(nc);
+                            current.Add(nc);
+                        }
+                    }
+                }
+            }
+            ret:
+            return result;
         }
         #endregion
         #region Key
@@ -106,17 +290,7 @@ namespace Berrysoft.Data
         #endregion
         #region Linq
         public static IEnumerable<TResult> SelectWhen<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, (bool Select, TResult Result)> predicate)
-        {
-            if (source == null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-            if (predicate == null)
-            {
-                throw new ArgumentNullException(nameof(predicate));
-            }
-            return SelectWhenIterator(source, predicate);
-        }
+            => SelectWhenIterator(source ?? throw new ArgumentNullException(nameof(source)), predicate ?? throw new ArgumentNullException(nameof(predicate)));
         private static IEnumerable<TResult> SelectWhenIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, (bool Select, TResult Result)> predicate)
         {
             foreach (TSource item in source)
@@ -126,6 +300,20 @@ namespace Berrysoft.Data
                 {
                     yield return Result;
                 }
+            }
+        }
+        public static IEnumerable<TSource> Random<TSource>(this IEnumerable<TSource> source)
+            => RandomIterator(source ?? throw new ArgumentNullException(nameof(source)));
+        private static IEnumerable<TSource> RandomIterator<TSource>(IEnumerable<TSource> source)
+        {
+            Random random = new Random();
+            List<TSource> list = new List<TSource>(source);
+            int n = list.Count;
+            for (int i = 0; i < n; i++)
+            {
+                int index = random.Next(list.Count - 1);
+                yield return list[index];
+                list.RemoveAt(index);
             }
         }
         #endregion
