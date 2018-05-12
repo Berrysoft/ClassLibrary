9cce5b1302aea66b1f3fd4600151cd4bafa98628
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-12 15:56:49 +0800

    Add Workflow.

---
+++ Berrysoft.Workflow/AsyncExecutable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Berrysoft.Workflow
+{
+    public class AsyncExecutable : IExecutable
+    {
+        Func<IExecutable> func;
+        Func<Task<IExecutable>> funcAsync;
+        public AsyncExecutable(Func<Task<IExecutable>> funcAsync)
+            : this(null, funcAsync)
+        { }
+        public AsyncExecutable(Func<IExecutable> func, Func<Task<IExecutable>> funcAsync)
+        {
+            this.func = func;
+            this.funcAsync = funcAsync;
+        }
+        public IExecutor GetExecutor()
+        {
+            throw new NotImplementedException();
+        }
+        internal struct Executor : IExecutor
+        {
+            AsyncExecutable executable;
+            public Executor(AsyncExecutable executable)
+            {
+                this.executable = executable;
+            }
+            public IExecutable Execute() => (executable.func ?? throw new NotImplementedException()).Invoke();
+            public Task<IExecutable> ExecuteAsync() => executable.funcAsync();
+        }
+    }
+}
