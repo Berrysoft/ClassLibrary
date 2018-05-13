4fc17c3a9df3b6205b985a5b9aecfcbe1b7fc7fe
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-13 09:37:56 +0800

    Add Workflow test.

---
+++ Berrysoft.Test/WorkflowTest.vb
@@ -0,0 +1,26 @@
+﻿Imports Berrysoft.Workflow
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()>
+Public Class WorkflowTest
+
+    <TestMethod()>
+    Public Sub IfTest()
+        Dim i As Integer = 0
+        Dim trueFunc As Func(Of IExecutable) =
+            Function()
+                Assert.AreEqual(i, 0)
+                Return Nothing
+            End Function
+        Dim falseFunc As Func(Of IExecutable) =
+            Function()
+                Assert.AreEqual(i, 1)
+                Return Nothing
+            End Function
+        Dim condition As New Condition(Function() i = 0, trueFunc, falseFunc)
+        condition.Work()
+        i = 1
+        condition.Work()
+    End Sub
+
+End Class
