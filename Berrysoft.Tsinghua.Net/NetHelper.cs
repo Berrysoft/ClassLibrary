7c0acb9a0356f1af1f6cfd7c6c28387e43354387
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-15 20:05:50 +0800

    Change NetHelper to http(https may crash).

--- Berrysoft.Tsinghua.Net/NetHelper.cs
+++ Berrysoft.Tsinghua.Net/NetHelper.cs
@@ -5,8 +5,8 @@ namespace Berrysoft.Tsinghua.Net
 {
     public class NetHelper : NetHelperBase, IConnect
     {
-        private const string LogUri = "https://net.tsinghua.edu.cn/do_login.php";
-        private const string FluxUri = "https://net.tsinghua.edu.cn/rad_user_info.php";
+        private const string LogUri = "http://net.tsinghua.edu.cn/do_login.php";
+        private const string FluxUri = "http://net.tsinghua.edu.cn/rad_user_info.php";
         private const string LoginData = "action=login&username={0}&password={1}&ac_id=1";
         private const string LogoutData = "action=logout";
         public NetHelper(string username, string password)
