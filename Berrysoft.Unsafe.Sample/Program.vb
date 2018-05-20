e65ca21c0cf1c166776fb00e9b8e787f45755255
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-20 14:54:54 +0800

    Add some...

--- Berrysoft.Unsafe.Sample/Program.vb
+++ Berrysoft.Unsafe.Sample/Program.vb
@@ -3,7 +3,7 @@ Module Program
         Dim int1 As Integer = 111
         Console.WriteLine("Init an Int32: {0}", int1)
         Dim ptr1 As Pointer(Of Integer) = UnsafeMethods.AddressOf(int1)
-        Console.WriteLine("The address of the Int32: 0x{0}", CType(ptr1, IntPtr).ToString("x16"))
+        Console.WriteLine("The address of the Int32: 0x{0}", ptr1.ToString("x16"))
         UnsafeMethods.TargetOf(ptr1) = 222
         Console.WriteLine("Now the Int32 is: {0}", int1)
         UnsafeMethods.StackAlloc(Of Integer)(2,
