d482ad2797eaa1e12ac39986c1b93b94b42cda0a
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-22 12:43:30 +0800

    Add FtpClient for Physics.

---
+++ Berrysoft.Tsinghua.Physics/FtpClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berrysoft.Tsinghua.Physics
+{
+    public class FtpClient
+    {
+        private const string BaseAddr = "ftp://166.111.26.13/";
+        private WebClient client;
+        public FtpClient()
+        {
+            client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            client.BaseAddress = BaseAddr;
+        }
+        public ICredentials Credentials
+        {
+            get => client.Credentials;
+            set => client.Credentials = value;
+        }
+        public void CancelAsync() => client.CancelAsync();
+        public void DownloadFile(string address, string fileName) => client.DownloadFile(address, fileName);
+        public void DownloadFile(Uri address, string fileName) => client.DownloadFile(address, fileName);
+        public Task DownloadFileAsync(string address, string fileName) => client.DownloadFileTaskAsync(address, fileName);
+        public Task DownloadFileAsync(Uri address, string fileName) => client.DownloadFileTaskAsync(address, fileName);
+        public byte[] UploadFile(string address, string fileName) => client.UploadFile(address, fileName);
+        public byte[] UploadFile(Uri address, string fileName) => client.UploadFile(address, fileName);
+        public Task<byte[]> UploadFileAsync(string address, string fileName) => client.UploadFileTaskAsync(address, fileName);
+        public Task<byte[]> UploadFileAsync(Uri address, string fileName) => client.UploadFileTaskAsync(address, fileName);
+    }
+}
---
+++ Berrysoft.Tsinghua.Physics/KnownFtpFolders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Berrysoft.Tsinghua.Physics
+{
+    public static class KnownFtpFolders
+    {
+        public static readonly Uri Academic = new Uri("/Academic", UriKind.Relative);
+        public static readonly Uri Downloads = new Uri("/Downloads", UriKind.Relative);
+        public static readonly Uri Entertainment = new Uri("/Entertainment", UriKind.Relative);
+        public static readonly Uri Comic = new Uri("/Entertainment/Comic", UriKind.Relative);
+        public static readonly Uri Video = new Uri("/Entertainment/Video", UriKind.Relative);
+        public static readonly Uri Softwares = new Uri("/Softwares", UriKind.Relative);
+    }
+}
