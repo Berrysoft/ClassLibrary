041728198c808a487862e347c52d98626b0e39f4
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-27 11:41:43 +0800

    Add remarks.

--- src/Berrysoft.Data/Tree.cs
+++ src/Berrysoft.Data/Tree.cs
@@ -81,17 +81,34 @@ namespace Berrysoft.Data
         void Clear();
     }
     #endregion
+    /// <summary>
+    /// Represents a tree with a root <see cref="Node{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of value the node contains.</typeparam>
     public class Tree<T> : ITree<T, Node<T>>
     {
         private Node<T> _root;
+        /// <summary>
+        /// Initialize an instance of <see cref="Tree{T}"/>.
+        /// </summary>
         public Tree()
         {
             _root = new Node<T>();
         }
+        /// <summary>
+        /// Initialize an instance of <see cref="Tree{T}"/> with root value.
+        /// </summary>
+        /// <param name="value">The value of root node.</param>
         public Tree(T value)
         {
             _root = new Node<T>(value);
         }
+        /// <summary>
+        /// Initialize an instance of <see cref="Tree{T}"/> with root node.
+        /// </summary>
+        /// <param name="root">Root node.</param>
+        /// <exception cref="ArgumentNullException">When <paramref name="root"/> is null.</exception>
+        /// <exception cref="ArgumentException">When <paramref name="root"/> has a parent.</exception>
         public Tree(Node<T> root)
         {
             if (root == null)
@@ -104,84 +121,131 @@ namespace Berrysoft.Data
             }
             _root = root;
         }
+        /// <summary>
+        /// The root node of the tree.
+        /// </summary>
         public Node<T> Root => _root;
+        /// <summary>
+        /// Get depth of the tree.
+        /// </summary>
+        /// <returns>The depth of the tree.</returns>
         public int GetDepth()
         {
+            int GetDepthInternal(Node<T> node, int depth)
+            {
+                int result = depth;
+                foreach (Node<T> child in node.AsEnumerable())
+                {
+                    int tempDepth = GetDepthInternal(child, depth + 1);
+                    if (tempDepth > result)
+                    {
+                        result = tempDepth;
+                    }
+                }
+                return result;
+            }
             return GetDepthInternal(_root, 1);
         }
-        private int GetDepthInternal(Node<T> node, int depth)
-        {
-            int result = depth;
-            foreach (Node<T> child in node.AsEnumerable())
-            {
-                int tempDepth = GetDepthInternal(child, depth + 1);
-                if (tempDepth > result)
-                {
-                    result = tempDepth;
-                }
-            }
-            return result;
-        }
     }
+    /// <summary>
+    /// Represents a node of a <see cref="Tree{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of value the node contains.</typeparam>
     public class Node<T> : INode<T, Node<T>>
     {
         private T _value;
         private Node<T> _parent;
         private List<Node<T>> _children;
+        /// <summary>
+        /// Initialize an instance of <see cref="Node{T}"/>.
+        /// </summary>
         public Node()
             : this(default)
         { }
+        /// <summary>
+        /// Initialize an instance of <see cref="Node{T}"/> with value.
+        /// </summary>
+        /// <param name="value">Value of the node.</param>
         public Node(T value)
         {
             _value = value;
             _children = new List<Node<T>>();
         }
-        public Node(T value, Node<T> parent)
-        {
-            _value = value;
-            _parent = parent;
-            _children = new List<Node<T>>();
-        }
+        /// <summary>
+        /// Initialize an instance of <see cref="Node{T}"/> with value and children.
+        /// </summary>
+        /// <param name="value">Value of the node.</param>
+        /// <param name="children">Children of the node.</param>
         public Node(T value, IEnumerable<Node<T>> children)
         {
             _value = value;
-            _children = new List<Node<T>>(children);
+            _children = new List<Node<T>>(children.ForEach(SetParent));
         }
-        public Node(T value, Node<T> parent, IEnumerable<Node<T>> children)
+        /// <summary>
+        /// Set the child's parent as this node.
+        /// </summary>
+        /// <param name="child">Child to be set.</param>
+        private void SetParent(Node<T> child)
         {
-            _value = value;
-            _parent = parent;
-            _children = new List<Node<T>>(children);
+            (child ?? throw ExceptionHelper.ArgumentNull(nameof(child)))._parent = this;
         }
+        /// <summary>
+        /// Value of the node.
+        /// </summary>
         public T Value
         {
             get => _value;
             set => _value = value;
         }
+        /// <summary>
+        /// Parent of the node, null when the node is root node of a tree.
+        /// </summary>
         public Node<T> Parent => _parent;
+        /// <summary>
+        /// The count of children.
+        /// </summary>
         public int Count => _children.Count;
+        /// <summary>
+        /// Add a child.
+        /// </summary>
+        /// <param name="child">The child node to be added.</param>
         public void Add(Node<T> child)
         {
-            if (child == null)
-            {
-                throw ExceptionHelper.ArgumentNull(nameof(child));
-            }
-            child._parent = this;
+            SetParent(child);
             _children.Add(child);
         }
+        /// <summary>
+        /// Add children.
+        /// </summary>
+        /// <param name="children">The child nodes to be added.</param>
+        public void AddRange(IEnumerable<Node<T>> children)
+        {
+            _children.AddRange((children ?? throw ExceptionHelper.ArgumentNull(nameof(children))).ForEach(SetParent));
+        }
+        /// <summary>
+        /// Insert a child.
+        /// </summary>
+        /// <param name="index">The zero-based index at which child should be inserted.</param>
+        /// <param name="child">The child to insert.</param>
         public void Insert(int index, Node<T> child)
         {
-            if (child == null)
-            {
-                throw ExceptionHelper.ArgumentNull(nameof(child));
-            }
-            child._parent = this;
+            SetParent(child);
             _children.Insert(index, child);
         }
+        /// <summary>
+        /// Determines whether a child is in the <see cref="INode{TValue, TNode}"/>.
+        /// </summary>
+        /// <param name="child">The child to locate in the <see cref="INode{TValue, TNode}"/>.</param>
+        /// <returns>true if the child is found; otherwise, false.</returns>
         public bool Contains(Node<T> child)
         {
             return _children.Contains(child);
         }
+        /// <summary>
+        /// Remove a child.
+        /// </summary>
+        /// <param name="child">The child node to be removed.</param>
+        /// <returns>true if the child is successfully removed; otherwise, false.</returns>
         public bool Remove(Node<T> child)
         {
             if (_children.Remove(child))
@@ -191,14 +255,25 @@ namespace Berrysoft.Data
             }
             return false;
         }
+        /// <summary>
+        /// Clear all children.
+        /// </summary>
         public void Clear()
         {
             _children.ForEach(child => child._parent = null);
             _children.Clear();
         }
+        /// <summary>
+        /// Get an <see cref="IEnumerable{TNode}"/> of its children.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Node<T>> AsEnumerable() => _children;
         public static explicit operator T(Node<T> node) => node.Value;
         public static implicit operator Node<T>(T value) => new Node<T>(value);
+        /// <summary>
+        /// Returns a string that represents the value.
+        /// </summary>
+        /// <returns>A string that represents the value.</returns>
         public override string ToString()
         {
             return _value.ToString();
