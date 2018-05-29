635cf388dbeff170059b1ea162ddf0628cab7ed6
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-29 17:08:28 +0800

    Remarks of Unsafe.

--- src/Berrysoft.Unsafe/Pointer.cs
+++ src/Berrysoft.Unsafe/Pointer.cs
@@ -5,66 +5,188 @@ using static System.Runtime.CompilerServices.Unsafe;
 
 namespace Berrysoft.Unsafe
 {
+    /// <summary>
+    /// Represents a .NET CLR pointer.
+    /// </summary>
+    /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+    /// <example>
+    /// This example shows how to use pointer in Visual Basic.
+    /// <code language="VB"><![CDATA[
+    /// Imports Berrysoft.Unsafe.UnsafeMethods
+    /// Module Program
+    ///     Sub Main(args As String())
+    ///         Dim int1 As Integer = 111
+    ///         Console.WriteLine("Init an Int32: {0}", int1)
+    ///         Dim ptr1 As Pointer(Of Integer) = PointerOf(int1)
+    ///         Console.WriteLine("The address of the Int32: 0x{0}", ptr1.ToString("x16"))
+    ///         TargetOf(ptr1) = 222
+    ///         Console.WriteLine("Now the Int32 is: {0}", int1)
+    ///         StackAlloc(Of Integer)(2,
+    ///             Sub(ptr2)
+    ///                 Console.WriteLine("Allocated two Int32: {0}, {1}", ptr2(0), ptr2(1))
+    ///                 MemorySet(ptr2, &H33, 4)
+    ///                 MemorySet(ptr2 + 1, &H22, 4)
+    ///                 Console.WriteLine("Now they are: 0x{0}, 0x{1}", ptr2(0).ToString("x8"), ptr2(1).ToString("x8"))
+    ///                 MemoryCopy(ptr1, ptr2, 4)
+    ///             End Sub)
+    ///         Console.WriteLine("Now the first Int32 is: 0x{0}", int1.ToString("x8"))
+    ///     End Sub
+    /// End Module
+    /// ]]></code>
+    /// </example>
     [DebuggerDisplay("{Ptr}")]
     public readonly unsafe struct Pointer<T> : IEquatable<Pointer<T>>
     {
         private readonly void* _ptr;
+        /// <summary>
+        /// Represents a <see langword="null"/> pointer.
+        /// </summary>
         public static readonly Pointer<T> Null;
+        /// <summary>
+        /// Initialize a <see cref="Pointer{T}"/> with a <see cref="void"/> pointer.
+        /// </summary>
+        /// <param name="ptr">The pointer.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Pointer(void* ptr)
         {
             _ptr = ptr;
         }
+        /// <summary>
+        /// Initialize a <see cref="Pointer{T}"/> with an <see cref="IntPtr"/>.
+        /// </summary>
+        /// <param name="ptr">The pointer.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Pointer(IntPtr ptr)
         {
             _ptr = (void*)ptr;
         }
+        /// <summary>
+        /// Initialize a <see cref="Pointer{T}"/> with a <typeparamref name="T"/> reference.
+        /// </summary>
+        /// <param name="ptr">The reference.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Pointer(ref T ptr)
         {
             _ptr = AsPointer(ref ptr);
         }
+        /// <summary>
+        /// The contained pointer.
+        /// </summary>
         public void* Ptr
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _ptr;
         }
+        /// <summary>
+        /// The target reference of <see cref="Pointer{T}"/>.
+        /// </summary>
         public ref T Target
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => ref AsRef<T>(_ptr);
         }
-
+        /// <summary>
+        /// The target reference of <see cref="Pointer{T}"/> with offset.
+        /// </summary>
+        /// <param name="index">The offset.</param>
+        /// <returns>The target reference.</returns>
         public ref T this[int index]
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => ref Add(ref AsRef<T>(_ptr), index);
         }
+        /// <summary>
+        /// Adds offset to <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <param name="ptr">The pointer.</param>
+        /// <param name="offset">The offset.</param>
+        /// <returns>Added <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Pointer<T> operator +(Pointer<T> ptr, int offset) => new Pointer<T>(ref Add(ref AsRef<T>(ptr._ptr), offset));
+        /// <summary>
+        /// Subtracts offset to <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <param name="ptr">The pointer.</param>
+        /// <param name="offset">The offset.</param>
+        /// <returns>Subtracted <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Pointer<T> operator -(Pointer<T> ptr, int offset) => new Pointer<T>(ref Subtract(ref AsRef<T>(ptr._ptr), offset));
+        /// <summary>
+        /// Increses <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <param name="ptr">The pointer.</param>
+        /// <returns>Incresed <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Pointer<T> operator ++(Pointer<T> ptr) => ptr + 1;
+        /// <summary>
+        /// Decreses <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <param name="ptr">The pointer.</param>
+        /// <returns>Decresed <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Pointer<T> operator --(Pointer<T> ptr) => ptr - 1;
+        /// <summary>
+        /// Determines whether two <see cref="Pointer{T}"/> are equal.
+        /// </summary>
+        /// <param name="left">One pointer.</param>
+        /// <param name="right">Another pointer.</param>
+        /// <returns><see langword="true"/> if they are equal; otherwise, <see langword="false"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(Pointer<T> left, Pointer<T> right) => left._ptr == right._ptr;
+        /// <summary>
+        /// Determines whether two <see cref="Pointer{T}"/> aren't equal.
+        /// </summary>
+        /// <param name="left">One pointer.</param>
+        /// <param name="right">Another pointer.</param>
+        /// <returns><see langword="true"/> if they aren't equap; otherwise, <see langword="false"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(Pointer<T> left, Pointer<T> right) => left._ptr != right._ptr;
+        /// <summary>
+        /// Convert a <see cref="void"/> pointer to <see cref="Pointer{T}"/> implicitly.
+        /// </summary>
+        /// <param name="ptr">The <see cref="void"/> pointer.</param>
+        /// <returns>A new <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Pointer<T>(void* ptr) => new Pointer<T>(ptr);
+        /// <summary>
+        /// Convert a <see cref="Pointer{T}"/> to <see cref="void"/> pointer implicitly.
+        /// </summary>
+        /// <param name="ptr">The <see cref="Pointer{T}"/>.</param>
+        /// <returns>The contained <see cref="void"/> pointer.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator void*(Pointer<T> ptr) => ptr._ptr;
+        /// <summary>
+        /// Convert an <see cref="IntPtr"/> to <see cref="Pointer{T}"/> explicitly.
+        /// </summary>
+        /// <param name="ptr">The <see cref="IntPtr"/>.</param>
+        /// <returns>A new <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static explicit operator Pointer<T>(IntPtr ptr) => new Pointer<T>(ptr);
+        /// <summary>
+        /// Convert a <see cref="Pointer{T}"/> to <see cref="IntPtr"/> explicitly.
+        /// </summary>
+        /// <param name="ptr"></param>
+        /// <returns>A new <see cref="IntPtr"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static explicit operator IntPtr(Pointer<T> ptr) => (IntPtr)(ptr._ptr);
+        /// <summary>
+        /// Detenmines whether the pointer is <see langword="null"/>.
+        /// </summary>
+        /// <returns><see langword="true"/> if the pointer is <see langword="null"/>; otherwise, <see langword="false"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool IsNull() => _ptr == null;
+        /// <summary>
+        /// Convert this pointer to a new pointer of <typeparamref name="TTo"/>.
+        /// </summary>
+        /// <typeparam name="TTo">The target type of new pointer.</typeparam>
+        /// <returns>The new pointer.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Pointer<TTo> ToPointer<TTo>() => new Pointer<TTo>(_ptr);
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified object.
+        /// </summary>
+        /// <param name="obj">An object to compare with this instance or null.</param>
+        /// <returns><see langword="true"/> if <paramref name="obj"/> is an instance of <see cref="Pointer{T}"/> and equals the value of this instance; otherwise, <see langword="false"/>.</returns>
         public override bool Equals(object obj)
         {
             if (obj is Pointer<T> p)
@@ -73,9 +195,27 @@ namespace Berrysoft.Unsafe
             }
             return false;
         }
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified <see cref="Pointer{T}"/> value.
+        /// </summary>
+        /// <param name="other">An <see cref="Pointer{T}"/> value to compare to this instance.</param>
+        /// <returns><see langword="true"/> if <paramref name="other"/> has the same value as this instance; otherwise, <see langword="false"/>.</returns>
         public bool Equals(Pointer<T> other) => this == other;
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
         public override int GetHashCode() => ((IntPtr)_ptr).GetHashCode();
+        /// <summary>
+        /// Converts the numeric value of the current <see cref="Pointer{T}"/> object to its equivalent string representation.
+        /// </summary>
+        /// <returns>The string representation of the value of this instance.</returns>
         public override string ToString() => ((IntPtr)_ptr).ToString();
+        /// <summary>
+        /// Converts the numeric value of the current <see cref="Pointer{T}"/> object to its equivalent string representation.
+        /// </summary>
+        /// <param name="format">A format specification that governs how the current <see cref="Pointer{T}"/> object is converted.</param>
+        /// <returns>The string representation of the value of the current <see cref="Pointer{T}"/> object.</returns>
         public string ToString(string format) => ((IntPtr)_ptr).ToString(format);
     }
 }
--- src/Berrysoft.Unsafe/UnsafeMethods.cs
+++ src/Berrysoft.Unsafe/UnsafeMethods.cs
@@ -4,35 +4,95 @@ using static System.Runtime.CompilerServices.Unsafe;
 
 namespace Berrysoft.Unsafe
 {
+    /// <summary>
+    /// Contains generic, low-level functionality for <see cref="Pointer{T}"/> and <see cref="Unsafe.ByReference{T}"/>.
+    /// </summary>
     public unsafe static class UnsafeMethods
     {
+        /// <summary>
+        /// Get size of a <typeparamref name="T"/> instance.
+        /// </summary>
+        /// <typeparam name="T">The type to get size.</typeparam>
+        /// <returns>The size of an instance if <typeparamref name="T"/> is value type; otherwise, the size of pointer.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetSize<T>() => SizeOf<T>();
+        /// <summary>
+        /// Get a <see cref="Pointer{T}"/> with a <typeparamref name="T"/> reference.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="value">The reference.</param>
+        /// <returns>An instance of <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Pointer<T> AddressOf<T>(ref T value) => new Pointer<T>(ref value);
+        public static Pointer<T> PointerOf<T>(ref T value) => new Pointer<T>(ref value);
+        /// <summary>
+        /// Get a <see cref="Pointer{T}"/> of an array.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="array">The array.</param>
+        /// <returns>An instance of <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Pointer<T> AddressOf<T>(T[] array) => new Pointer<T>(ref array[0]);
+        public static Pointer<T> PointerOf<T>(T[] array) => new Pointer<T>(ref array[0]);
+        /// <summary>
+        /// Get a <see cref="Unsafe.ByReference{T}"/> with a <typeparamref name="T"/> reference.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="value">The reference.</param>
+        /// <returns>An instance of <see cref="Unsafe.ByReference{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ByReference<T> ByRef<T>(ref T value) => new ByReference<T>(ref value);
+        public static ByReference<T> ByReference<T>(ref T value) => new ByReference<T>(ref value);
+        /// <summary>
+        /// Get the target reference of <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="ptr">The <see cref="Pointer{T}"/>.</param>
+        /// <returns>The target reference of <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T TargetOf<T>(Pointer<T> ptr) => ref ptr.Target;
+        /// <summary>
+        /// Alloc an array on stack and do <paramref name="action"/> with the pointer.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="size">Size of the array.</param>
+        /// <param name="action">An <see cref="Action{T}"/> delegate do with the pointer.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void StackAlloc<T>(int size, Action<Pointer<T>> action)
         {
             byte* ptr = stackalloc byte[size * SizeOf<T>()];
             action(new Pointer<T>(ptr));
         }
+        /// <summary>
+        /// Get a <see cref="Span{T}"/> of a <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="ptr">The <see cref="Pointer{T}"/>.</param>
+        /// <param name="size">Size of the <see cref="Span{T}"/>.</param>
+        /// <returns>A <see cref="Span{T}"/> of a <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Span<T> AsSpan<T>(this Pointer<T> ptr, int size) => new Span<T>(ptr.Ptr, size);
+        /// <summary>
+        /// Get a <see cref="ReadOnlySpan{T}"/> of a <see cref="Pointer{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the pointer pointed to.</typeparam>
+        /// <param name="ptr">The <see cref="Pointer{T}"/>.</param>
+        /// <param name="size">Size of the <see cref="ReadOnlySpan{T}"/>.</param>
+        /// <returns>A <see cref="ReadOnlySpan{T}"/> of a <see cref="Pointer{T}"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySpan<T> AsReadOnlySpan<T>(this Pointer<T> ptr, int size) => new ReadOnlySpan<T>(ptr.Ptr, size);
+        /// <summary>
+        /// Initializes a block of memory at the given location with a given initial value.
+        /// </summary>
+        /// <param name="startAddress">The address of the start of the memory block to initialize.</param>
+        /// <param name="value">The value to initialize the block to.</param>
+        /// <param name="byteCount">The number of bytes to initialize.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MemorySet(IntPtr startAddress, byte value, uint byteCount) => InitBlock((void*)startAddress, value, byteCount);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void MemorySet<T>(Pointer<T> startAddress, byte value, uint byteCount) => InitBlock(startAddress, value, byteCount);
+        /// <summary>
+        /// Copies bytes from the <paramref name="source"/> address to the <paramref name="destination"/> address.
+        /// </summary>
+        /// <param name="destination">The destination address to copy to.</param>
+        /// <param name="source">The source address to copy from.</param>
+        /// <param name="byteCount">The number of bytes to copy.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MemoryCopy(IntPtr destination, IntPtr source, uint byteCount) => CopyBlock((void*)destination, (void*)source, byteCount);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void MemoryCopy<TDestination, TSource>(Pointer<TDestination> destination, Pointer<TSource> source, uint byteCount) => CopyBlock(destination, source, byteCount);
     }
 }
