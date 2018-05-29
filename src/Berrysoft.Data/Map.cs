95e4398aae413e5e94388d886d5333848d6958b8
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-29 23:11:11 +0800

    Rewrite Map with two Dictionary.

--- src/Berrysoft.Data/Map.cs
+++ src/Berrysoft.Data/Map.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace Berrysoft.Data
@@ -15,13 +14,13 @@ namespace Berrysoft.Data
     public interface IMap<TKey1, TKey2> : ICollection<KeyPair<TKey1, TKey2>>
     {
         /// <summary>
-        /// Gets an <see cref="IEnumerable{TKey1}"/> containing the keys in the <see cref="IMap{TKey1, TKey2}"/>.
+        /// Gets an <see cref="ICollection{TKey1}"/> containing the keys in the <see cref="IMap{TKey1, TKey2}"/>.
         /// </summary>
-        IEnumerable<TKey1> Keys1 { get; }
+        ICollection<TKey1> Keys1 { get; }
         /// <summary>
-        /// Gets an <see cref="IEnumerable{TKey2}"/> containing the keys in the <see cref="IMap{TKey1, TKey2}"/>.
+        /// Gets an <see cref="ICollection{TKey2}"/> containing the keys in the <see cref="IMap{TKey1, TKey2}"/>.
         /// </summary>
-        IEnumerable<TKey2> Keys2 { get; }
+        ICollection<TKey2> Keys2 { get; }
         /// <summary>
         /// Get key2 with the specified key1.
         /// </summary>
@@ -59,8 +58,7 @@ namespace Berrysoft.Data
         /// </summary>
         /// <param name="key1">The key1 of the element to add.</param>
         /// <param name="key2">The key2 of the element to add.</param>
-        /// <returns><see langword="true"/> if key1 or key2 is/are not contained in the <see cref="IMap{TKey1, TKey2}"/> and set successfully; otherwise, <see langword="false"/>.</returns>
-        bool SetPair(TKey1 key1, TKey2 key2);
+        void SetPair(TKey1 key1, TKey2 key2);
         /// <summary>
         /// Determines whether the <see cref="IMap{TKey1, TKey2}"/> contains the specified key1.
         /// </summary>
@@ -204,9 +202,8 @@ namespace Berrysoft.Data
     /// <typeparam name="TKey2">Type of key2.</typeparam>
     public class Map<TKey1, TKey2> : IMap<TKey1, TKey2>
     {
-        private List<KeyPair<TKey1, TKey2>> list;
-        private IEqualityComparer<TKey1> comparer1;
-        private IEqualityComparer<TKey2> comparer2;
+        private Dictionary<TKey1, TKey2> dic;
+        private Dictionary<TKey2, TKey1> rev;
         /// <summary>
         /// Initialize a new instance of <see cref="Map{TKey1, TKey2}"/>.
         /// </summary>
@@ -236,9 +233,8 @@ namespace Berrysoft.Data
         /// <param name="comparer2">Comparer of key2.</param>
         public Map(int capacity, IEqualityComparer<TKey1> comparer1, IEqualityComparer<TKey2> comparer2)
         {
-            this.list = new List<KeyPair<TKey1, TKey2>>(capacity);
-            this.comparer1 = comparer1 ?? EqualityComparer<TKey1>.Default;
-            this.comparer2 = comparer2 ?? EqualityComparer<TKey2>.Default;
+            this.dic = new Dictionary<TKey1, TKey2>(capacity, comparer1);
+            this.rev = new Dictionary<TKey2, TKey1>(capacity, comparer2);
         }
         /// <summary>
         /// Initialize a new instance of <see cref="Map{TKey1, TKey2}"/>.
@@ -264,207 +260,92 @@ namespace Berrysoft.Data
         /// <summary>
         /// Count of elements.
         /// </summary>
-        public int Count => list.Count;
+        public int Count => dic.Count;
         /// <summary>
         /// Gets a value indicating whether the <see cref="Map{TKey1, TKey2}"/> is read-only.
         /// </summary>
         /// <value><see langword="false"/></value>
         public bool IsReadOnly => false;
         /// <summary>
-        /// Gets an <see cref="IEnumerable{TKey1}"/> containing the keys in the <see cref="Map{TKey1, TKey2}"/>.
+        /// Gets an <see cref="ICollection{TKey1}"/> containing the keys in the <see cref="Map{TKey1, TKey2}"/>.
         /// </summary>
-        public IEnumerable<TKey1> Keys1 => list.Select(pair => pair.Key1);
+        public Dictionary<TKey1, TKey2>.KeyCollection Keys1 => dic.Keys;
+        ICollection<TKey1> IMap<TKey1, TKey2>.Keys1 => Keys1;
         /// <summary>
-        /// Gets an <see cref="IEnumerable{TKey2}"/> containing the keys in the <see cref="Map{TKey1, TKey2}"/>.
+        /// Gets an <see cref="ICollection{TKey2}"/> containing the keys in the <see cref="Map{TKey1, TKey2}"/>.
         /// </summary>
-        public IEnumerable<TKey2> Keys2 => list.Select(pair => pair.Key2);
-        private bool Insert(TKey1 key1, TKey2 key2, bool add)
-        {
-            int index1 = -1;
-            int index2 = -1;
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (comparer1.Equals(list[i].Key1, key1))
-                {
-                    if (add)
-                    {
-                        throw ExceptionHelper.KeyExisted($"Key1 {key1}");
-                    }
-                    index1 = i;
-                }
-                if (comparer2.Equals(list[i].Key2, key2))
-                {
-                    if (add)
-                    {
-                        throw ExceptionHelper.KeyExisted($"Key2 {key2}");
-                    }
-                    index2 = i;
-                }
-            }
-            if (index1 == index2)
-            {
-                if (index1 == -1)
-                {
-                    list.Add(new KeyPair<TKey1, TKey2>(key1, key2));
-                }
-                else if (add)
-                {
-                    throw ExceptionHelper.PairExisted();
-                }
-            }
-            else
-            {
-                if (index1 == -1)
-                {
-                    list[index2] = new KeyPair<TKey1, TKey2>(key1, key2);
-                }
-                else if (index2 == -1)
-                {
-                    list[index1] = new KeyPair<TKey1, TKey2>(key1, key2);
-                }
-                else if (add)
-                {
-                    throw ExceptionHelper.KeysExisted();
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+        public Dictionary<TKey2, TKey1>.KeyCollection Keys2 => rev.Keys;
+        ICollection<TKey2> IMap<TKey1, TKey2>.Keys2 => Keys2;
         /// <summary>
         /// Adds the specified key1 and key2.
         /// </summary>
         /// <param name="key1">The key1 of the element to add.</param>
         /// <param name="key2">The key2 of the element to add.</param>
-        public void Add(TKey1 key1, TKey2 key2) => Insert(key1, key2, true);
+        public void Add(TKey1 key1, TKey2 key2)
+        {
+            dic.Add(key1, key2);
+            rev.Add(key2, key1);
+        }
         /// <summary>
         /// Adds a <see cref="KeyPair{TKey1, TKey2}"/>.
         /// </summary>
         /// <param name="item">The specified key pair.</param>
-        public void Add(KeyPair<TKey1, TKey2> item) => Insert(item.Key1, item.Key2, true);
+        public void Add(KeyPair<TKey1, TKey2> item) => Add(item.Key1, item.Key2);
         /// <summary>
         /// Sets the specified key1 and key2 related.
         /// </summary>
         /// <param name="key1">The key1 of the element to add.</param>
         /// <param name="key2">The key2 of the element to add.</param>
-        /// <returns><see langword="true"/> if key1 or key2 is/are not contained in the <see cref="Map{TKey1, TKey2}"/> and set successfully; otherwise, <see langword="false"/>.</returns>
-        public bool SetPair(TKey1 key1, TKey2 key2) => Insert(key1, key2, false);
+        public void SetPair(TKey1 key1, TKey2 key2)
+        {
+            dic[key1] = key2;
+            rev[key2] = key1;
+        }
         /// <summary>
         /// Get key2 with the specified key1.
         /// </summary>
         /// <param name="key">The key1 of the key2 to get.</param>
         /// <returns>Key2 related to the specified key1.</returns>
-        public TKey2 GetValueFromKey1(TKey1 key)
-        {
-            if (TryGetValueFromKey1(key, out var value))
-            {
-                return value;
-            }
-            else
-            {
-                throw ExceptionHelper.KeyNotFound();
-            }
-        }
+        public TKey2 GetValueFromKey1(TKey1 key) => dic[key];
         /// <summary>
         /// Get key1 with the specified key2.
         /// </summary>
         /// <param name="key">The key2 of the key1 to get.</param>
         /// <returns>Key1 related to the specified key2.</returns>
-        public TKey1 GetValueFromKey2(TKey2 key)
-        {
-            if (TryGetValueFromKey2(key, out var value))
-            {
-                return value;
-            }
-            else
-            {
-                throw ExceptionHelper.KeyNotFound();
-            }
-        }
+        public TKey1 GetValueFromKey2(TKey2 key) => rev[key];
         /// <summary>
         /// Gets the key2 associated with the specified key1.
         /// </summary>
         /// <param name="key">The key1 of the key2 to get.</param>
         /// <param name="value">When this method returns, contains the key2 associated with the specified key1, if the key1 is found; otherwise, the default value for the type of the <paramref name="value"/> parameter. This parameter is passed uninitialized.</param>
         /// <returns><see langword="true"/> if the <see cref="Map{TKey1, TKey2}"/> contains an element with the specified key1; otherwise, <see langword="false"/>.</returns>
-        public bool TryGetValueFromKey1(TKey1 key, out TKey2 value)
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (comparer1.Equals(list[i].Key1, key))
-                {
-                    value = list[i].Key2;
-                    return true;
-                }
-            }
-            value = default;
-            return false;
-        }
+        public bool TryGetValueFromKey1(TKey1 key, out TKey2 value) => dic.TryGetValue(key, out value);
         /// <summary>
         /// Gets the key1 associated with the specified key2.
         /// </summary>
         /// <param name="key">The key2 of the key1 to get.</param>
         /// <param name="value">When this method returns, contains the key1 associated with the specified key2, if the key2 is found; otherwise, the default value for the type of the <paramref name="value"/> parameter. This parameter is passed uninitialized.</param>
         /// <returns><see langword="true"/> if the <see cref="Map{TKey1, TKey2}"/> contains an element with the specified key2; otherwise, <see langword="false"/>.</returns>
-        public bool TryGetValueFromKey2(TKey2 key, out TKey1 value)
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (comparer2.Equals(list[i].Key2, key))
-                {
-                    value = list[i].Key1;
-                    return true;
-                }
-            }
-            value = default;
-            return false;
-        }
+        public bool TryGetValueFromKey2(TKey2 key, out TKey1 value) => rev.TryGetValue(key, out value);
         /// <summary>
         /// Determines whether the <see cref="Map{TKey1, TKey2}"/> contains the specified key1.
         /// </summary>
         /// <param name="key">The specified key1.</param>
         /// <returns><see langword="true"/> if it contains; otherwise, <see langword="false"/>.</returns>
-        public bool ContainsKey1(TKey1 key)
-        {
-            if (key == null)
-            {
-                throw ExceptionHelper.ArgumentNull(nameof(key));
-            }
-            return list.FindIndex(pair => comparer1.Equals(pair.Key1, key)) >= 0;
-        }
+        public bool ContainsKey1(TKey1 key) => dic.ContainsKey(key);
         /// <summary>
         /// Determines whether the <see cref="Map{TKey1, TKey2}"/> contains the specified key2.
         /// </summary>
         /// <param name="key">The specified key2.</param>
         /// <returns><see langword="true"/> if it contains; otherwise, <see langword="false"/>.</returns>
-        public bool ContainsKey2(TKey2 key)
-        {
-            if (key == null)
-            {
-                throw ExceptionHelper.ArgumentNull(nameof(key));
-            }
-            return list.FindIndex(pair => comparer2.Equals(pair.Key2, key)) >= 0;
-        }
+        public bool ContainsKey2(TKey2 key) => rev.ContainsKey(key);
         /// <summary>
         /// Determines whether the <see cref="Map{TKey1, TKey2}"/> contains the element with specified key1 and key2.
         /// </summary>
         /// <param name="key1">The specified key1.</param>
         /// <param name="key2">The specified key2.</param>
         /// <returns><see langword="true"/> if it contains; otherwise, <see langword="false"/>.</returns>
-        public bool Contains(TKey1 key1, TKey2 key2)
-        {
-            if (key1 == null)
-            {
-                throw ExceptionHelper.ArgumentNull(nameof(key1));
-            }
-            if (key2 == null)
-            {
-                throw ExceptionHelper.ArgumentNull(nameof(key2));
-            }
-            return list.FindIndex(pair => comparer1.Equals(pair.Key1, key1) && comparer2.Equals(pair.Key2, key2)) >= 0;
-        }
+        public bool Contains(TKey1 key1, TKey2 key2) => dic.ContainsKey(key1) && rev.Comparer.Equals(dic[key1], key2);
         /// <summary>
         /// Determines whether the <see cref="Map{TKey1, TKey2}"/> contains the element.
         /// </summary>
@@ -477,36 +358,14 @@ namespace Berrysoft.Data
         /// <param name="key">The specified key1.</param>
         /// <returns><see langword="true"/> if removes successfully; otherwise, <see langword="false"/>.</returns>
         public bool RemoveKey1(TKey1 key)
-        {
-            int index = list.FindIndex(pair => comparer1.Equals(pair.Key1, key));
-            if (index < 0)
-            {
-                return false;
-            }
-            else
-            {
-                list.RemoveAt(index);
-                return true;
-            }
-        }
+            => dic.TryGetValue(key, out TKey2 value) && dic.Remove(key) && rev.Remove(value);
         /// <summary>
         /// Remove element with specified key2.
         /// </summary>
         /// <param name="key">The specified key2.</param>
         /// <returns><see langword="true"/> if removes successfully; otherwise, <see langword="false"/>.</returns>
         public bool RemoveKey2(TKey2 key)
-        {
-            int index = list.FindIndex(pair => comparer2.Equals(pair.Key2, key));
-            if (index < 0)
-            {
-                return false;
-            }
-            else
-            {
-                list.RemoveAt(index);
-                return true;
-            }
-        }
+            => rev.TryGetValue(key, out TKey1 value) && rev.Remove(key) && dic.Remove(value);
         /// <summary>
         /// Remove element with specified key1 and key2.
         /// </summary>
@@ -514,18 +373,7 @@ namespace Berrysoft.Data
         /// <param name="key2">The specified key2.</param>
         /// <returns><see langword="true"/> if removes successfully; otherwise, <see langword="false"/>.</returns>
         public bool Remove(TKey1 key1, TKey2 key2)
-        {
-            int index = list.FindIndex(pair => comparer1.Equals(pair.Key1, key1) && comparer2.Equals(pair.Key2, key2));
-            if (index < 0)
-            {
-                return false;
-            }
-            else
-            {
-                list.RemoveAt(index);
-                return true;
-            }
-        }
+            => dic.TryGetValue(key1, out TKey2 value) && rev.Comparer.Equals(value, key2) && dic.Remove(key1) && rev.Remove(key2);
         /// <summary>
         /// Remove an element.
         /// </summary>
@@ -535,32 +383,64 @@ namespace Berrysoft.Data
         /// <summary>
         /// Clear all elements.
         /// </summary>
-        public void Clear() => list.Clear();
+        public void Clear()
+        {
+            dic.Clear();
+            rev.Clear();
+        }
         /// <summary>
         /// Copies the entire <see cref="Map{TKey1, TKey2}"/> to a compatible one-dimensional array, starting at the specified index of the target array.
         /// </summary>
         /// <param name="array">The one-dimensional <see cref="Array"/> that is the destination of the elements copied from <see cref="Map{TKey1, TKey2}"/>. The <see cref="Array"/> must have zero-based indexing.</param>
         /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
-        public void CopyTo(KeyPair<TKey1, TKey2>[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
+        public void CopyTo(KeyPair<TKey1, TKey2>[] array, int arrayIndex)
+        {
+            foreach(var pair in dic)
+            {
+                array[arrayIndex] = new KeyPair<TKey1, TKey2>(pair.Key, pair.Value);
+                arrayIndex++;
+            }
+        }
+        public Enumerator GetEnumerator() => new Enumerator(this);
         /// <summary>
         /// Returns an enumerator that iterates through the <see cref="Map{TKey1, TKey2}"/>.
         /// </summary>
         /// <returns>An enumerator for the <see cref="Map{TKey1, TKey2}"/>.</returns>
-        public IEnumerator<KeyPair<TKey1, TKey2>> GetEnumerator() => list.GetEnumerator();
+        IEnumerator<KeyPair<TKey1, TKey2>> IEnumerable<KeyPair<TKey1, TKey2>>.GetEnumerator() => GetEnumerator();
         /// <summary>
         /// Returns an enumerator that iterates through the <see cref="Map{TKey1, TKey2}"/>.
         /// </summary>
         /// <returns>An enumerator for the <see cref="Map{TKey1, TKey2}"/>.</returns>
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)list).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         /// <summary>
         /// Get <see cref="Dictionary{TKey, TValue}"/> with key1 as key and key2 as value.
         /// </summary>
         /// <returns>An instance of <see cref="Dictionary{TKey, TValue}"/>.</returns>
-        public Dictionary<TKey1, TKey2> ToDictionaryFromKey1() => list.ToDictionary(pair => pair.Key1, pair => pair.Key2, comparer1);
+        public Dictionary<TKey1, TKey2> ToDictionaryFromKey1() => new Dictionary<TKey1, TKey2>(dic);
         /// <summary>
         /// Get <see cref="Dictionary{TKey, TValue}"/> with key2 as key and key1 as value.
         /// </summary>
         /// <returns>An instance of <see cref="Dictionary{TKey, TValue}"/>.</returns>
-        public Dictionary<TKey2, TKey1> ToDictionaryFromKey2() => list.ToDictionary(pair => pair.Key2, pair => pair.Key1, comparer2);
+        public Dictionary<TKey2, TKey1> ToDictionaryFromKey2() => new Dictionary<TKey2, TKey1>(rev);
+        public struct Enumerator : IEnumerator<KeyPair<TKey1, TKey2>>
+        {
+            private Dictionary<TKey1, TKey2>.Enumerator enumerator;
+            internal Enumerator(Map<TKey1, TKey2> map)
+            {
+                enumerator = map.dic.GetEnumerator();
+            }
+            public KeyPair<TKey1, TKey2> Current
+            {
+                get
+                {
+                    var current = enumerator.Current;
+                    return new KeyPair<TKey1, TKey2>(current.Key, current.Value);
+                }
+            }
+            object IEnumerator.Current => Current;
+            public void Dispose() => enumerator.Dispose();
+            public bool MoveNext() => enumerator.MoveNext();
+            void IEnumerator.Reset() => ((IEnumerator)enumerator).Reset();
+        }
     }
 }
