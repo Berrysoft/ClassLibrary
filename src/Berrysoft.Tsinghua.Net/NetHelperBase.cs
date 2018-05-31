aaf29503c63b7a263ffb699131c2d6371df384fb
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-31 21:35:41 +0800

    Some small changes.

--- src/Berrysoft.Tsinghua.Net/NetHelperBase.cs
+++ src/Berrysoft.Tsinghua.Net/NetHelperBase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -216,42 +215,5 @@ namespace Berrysoft.Tsinghua.Net
         /// </summary>
         public void Dispose() => Dispose(true);
         #endregion
-    }
-    internal static class CryptographyHelper
-    {
-        private static string GetHexString(byte[] data)
-        {
-            StringBuilder sBuilder = new StringBuilder();
-            for (int i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-            return sBuilder.ToString();
-        }
-        public static string GetMD5(string input)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                return string.Empty;
-            }
-            using (MD5 md5Hash = MD5.Create())
-            {
-                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-                return GetHexString(data);
-            }
-        }
-        public static string GetSHA1(string input)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                return string.Empty;
-            }
-            using (SHA1 sha1 = SHA1.Create())
-            {
-                const int ISO_8859_1 = 28591;
-                byte[] data = sha1.ComputeHash(Encoding.GetEncoding(ISO_8859_1).GetBytes(input));
-                return GetHexString(data);
-            }
-        }
     }
 }
