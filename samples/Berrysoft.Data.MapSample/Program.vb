34333cce22819f75dc8cae007e5f0012d2e01bf1
王宇逸 <Strawberry_Str@hotmail.com>
2018-06-11 22:48:56 +0800

    Add remarks for MapSample.

--- samples/Berrysoft.Data.MapSample/Program.vb
+++ samples/Berrysoft.Data.MapSample/Program.vb
@@ -16,12 +16,25 @@
         dictionary.Add("潮"c, "chao")
         dictionary.Add("常"c, "chang")
 
+        'This is a famous Chinese couplet of Temple of Lady Meng Jiangnu in Shanhaiguan.
+        'The correct pronunciation of it is:
+        'Hǎi  shuǐ  cháo   zhāo  zhāo  cháo   zhāo  cháo  zhāo  luò
+        '海    水    朝  /  朝    朝    朝  /  朝    朝    朝    落
+        'Fú   yún  zhǎng   cháng cháng zhǎng  cháng zhǎng cháng xiāo
+        '浮    云    长  /  长    长    长  /  长    长    长    消
+
+        '朝 means "day" when pronounces zhāo and means "tide" when pronounces cháo. Now the latter meaning is usually written as 潮.
+        '长 means "grow" when pronounces zhǎng and means "always" when pronounces cháng. Now the latter meaning is usually written as 常.
+
+        'This program means to replace the old multi-tone characters to their commonly used forms for easier understanding.
+
         Dim str = "海水朝朝朝朝朝朝朝落浮云长长长长长长长消".ToArray()
         Console.WriteLine(str)
 
         For i As Integer = 0 To str.Length - 1
             Dim pronounce = dictionary.GetValuesFromKey1(str(i))
             If pronounce.Count > 1 Then
+                Console.Write("{0}" + vbTab, str(i))
                 For j As Integer = 0 To pronounce.Count - 1
                     Console.Write("{0} {1}" + vbTab, j, pronounce(j))
                 Next
@@ -34,6 +47,9 @@
             End If
         Next
 
+        'If you enter the correct number: 0 1 1 0 1 0 1  0 1 1 0 1 0 1
+        'You will get: 海水潮朝朝潮朝潮朝落浮云长常常长常长常消
+        'Now it is easier to understand.
         Console.WriteLine(str)
     End Sub
 End Module
