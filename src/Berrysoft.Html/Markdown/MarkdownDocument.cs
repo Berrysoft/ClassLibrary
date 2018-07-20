a330d7bc7697f05a3aae3f82782424d7e36328cf
王宇逸 <Strawberry_Str@hotmail.com>
2018-07-20 15:06:54 +0800

    Markdown success!

--- src/Berrysoft.Html/Markdown/MarkdownDocument.cs
+++ src/Berrysoft.Html/Markdown/MarkdownDocument.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Berrysoft.Html.Markdown
 {
@@ -11,11 +12,20 @@ namespace Berrysoft.Html.Markdown
         public static HtmlDocument LoadAsHtml(string path)
         {
             List<string> lines = new List<string>();
+            List<MarkdownLineToken> tokens = new List<MarkdownLineToken>();
             using (StreamReader reader = new StreamReader(path))
             {
-                lines.AddRange(ReadLines(reader));
+                int index = 0;
+                while(!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    lines.Add(line);
+                    MarkdownLineToken token = GetToken(line);
+                    token.Line = index;
+                    tokens.Add(token);
+                    index++;
+                }
             }
-            List<MarkdownLineToken> tokens = new List<MarkdownLineToken>(GetTokens(lines));
             Stack<HtmlNode> currents = new Stack<HtmlNode>();
             List<HtmlObject> temp = new List<HtmlObject>();
             HtmlDocument document = new HtmlDocument();
@@ -24,9 +34,9 @@ namespace Berrysoft.Html.Markdown
             MarkdownLineTokenType lineToken = tokens[0].Type;
             foreach (MarkdownLineToken token in tokens)
             {
-                if (lineToken != token.Type)
+                if (lineToken == MarkdownLineTokenType.CodeBlock)
                 {
-                    if (lineToken != MarkdownLineTokenType.None)
+                    if (token.Type == MarkdownLineTokenType.CodeBlock)
                     {
                         HtmlNode current = currents.Pop();
                         foreach (HtmlObject o in temp)
@@ -34,49 +44,65 @@ namespace Berrysoft.Html.Markdown
                             current.AddElement(o);
                         }
                         currents.Peek().AddElement(current);
-                        if (lineToken == MarkdownLineTokenType.CodeBlock)
-                        {
-                            current = currents.Pop();
-                            currents.Peek().AddElement(current);
-                        }
+                        current = currents.Pop();
+                        currents.Peek().AddElement(current);
                         temp.Clear();
+                        lineToken = MarkdownLineTokenType.None;
                     }
-                    switch (token.Type)
+                    if (token.Tokens.Length > 0)
                     {
-                        case MarkdownLineTokenType.Paragraph:
-                            currents.Push(new HtmlNode("p"));
-                            break;
-                        case MarkdownLineTokenType.List:
-                            currents.Push(new HtmlNode("ul"));
-                            break;
-                        case MarkdownLineTokenType.CodeBlock:
-                            currents.Push(new HtmlNode("pre"));
-                            currents.Push(new HtmlNode("code"));
-                            break;
+                        temp.Add(lines[token.Line]);
+                        temp.Add(Environment.NewLine);
                     }
                 }
-                lineToken = token.Type;
-                IEnumerable<HtmlObject> objects = null;
-                if (token.Tokens.Length > 0)
+                else
                 {
-                    switch (token.Tokens[0].Type)
+                    if (lineToken != token.Type)
                     {
-                        case MarkdownTokenType.Head:
-                            objects = GetHtmlObjects(lines[token.Line].Substring(token.Tokens[0].Index + 1), token.Tokens.Skip(1));
-                            currents.Push(new HtmlNode($"h{token.Tokens[0].Index + 1}", objects));
-                            break;
-                        case MarkdownTokenType.ListItem:
-                            objects = GetHtmlObjects(lines[token.Line].Substring(token.Tokens[0].Index + 1), token.Tokens.Skip(1));
-                            temp.Add(new HtmlNode("li", objects));
-                            break;
-                        case MarkdownTokenType.Code:
-                            temp.Add(lines[token.Line]);
-                            temp.Add(Environment.NewLine);
-                            break;
-                        default:
-                            objects = GetHtmlObjects(lines[token.Line], token.Tokens);
-                            temp.AddRange(objects);
-                            break;
+                        if (lineToken != MarkdownLineTokenType.None)
+                        {
+                            HtmlNode current = currents.Pop();
+                            foreach (HtmlObject o in temp)
+                            {
+                                current.AddElement(o);
+                            }
+                            currents.Peek().AddElement(current);
+                            temp.Clear();
+                        }
+                        switch (token.Type)
+                        {
+                            case MarkdownLineTokenType.Paragraph:
+                                currents.Push(new HtmlNode("p"));
+                                break;
+                            case MarkdownLineTokenType.List:
+                                currents.Push(new HtmlNode("ul"));
+                                break;
+                            case MarkdownLineTokenType.CodeBlock:
+                                currents.Push(new HtmlNode("pre"));
+                                currents.Push(new HtmlNode("code"));
+                                break;
+                        }
+                    }
+                    lineToken = token.Type;
+                    IEnumerable<HtmlObject> objects = null;
+                    if (token.Tokens.Length > 0)
+                    {
+                        switch (token.Tokens[0].Type)
+                        {
+                            case MarkdownTokenType.Head:
+                                objects = GetHtmlObjects(lines[token.Line].Substring(token.Tokens[0].Index + 1), token.Tokens.Skip(1));
+                                currents.Push(new HtmlNode($"h{token.Tokens[0].Index + 1}", objects));
+                                break;
+                            case MarkdownTokenType.ListItem:
+                                objects = GetHtmlObjects(lines[token.Line].Substring(token.Tokens[0].Index + 1), token.Tokens.Skip(1));
+                                temp.Add(new HtmlNode("li", objects));
+                                break;
+                            default:
+                                objects = GetHtmlObjects(lines[token.Line], token.Tokens);
+                                temp.AddRange(objects);
+                                temp.Add(" ");
+                                break;
+                        }
                     }
                 }
             }
@@ -96,132 +122,134 @@ namespace Berrysoft.Html.Markdown
         private static IEnumerable<HtmlObject> GetHtmlObjects(string line, IEnumerable<MarkdownToken> tokens)
         {
             int startIndex = 0;
-            bool in_code = false;
             foreach (MarkdownToken token in tokens)
             {
                 switch (token.Type)
                 {
                     case MarkdownTokenType.Code:
-                        startIndex = token.Index + 1;
-                        in_code = !in_code;
+                        yield return new HtmlNode("code", line.Substring(startIndex, token.Index - startIndex + 1));
                         break;
                     case MarkdownTokenType.Text:
-                        if (in_code)
-                        {
-                            yield return new HtmlNode("code", line.Substring(startIndex, token.Index - startIndex + 1));
-                        }
-                        else
-                        {
-                            yield return line.Substring(startIndex, token.Index - startIndex + 1);
-                        }
+                        yield return line.Substring(startIndex, token.Index - startIndex + 1);
                         break;
                 }
+                startIndex = token.Index + 1;
             }
         }
 
+        private static readonly Regex HeadRegex = new Regex("[ ]*(#+[ ]+)([^#]+)#*");
+        private static readonly Regex ListItemRegex = new Regex(@"[ ]*(\*[ ]+)(.*)");
+        private static readonly Regex CodeBlockRegex = new Regex(@"(\`\`\`)(.*)");
+        private static MarkdownLineToken GetToken(string line)
+        {
+            List<MarkdownToken> tokensList = new List<MarkdownToken>();
+            MarkdownLineToken lineToken = default;
+            tokensList.Clear();
+            if (line.Length == 0)
+            {
+                lineToken.Type = MarkdownLineTokenType.None;
+            }
+            else if (HeadRegex.IsMatch(line))
+            {
+                lineToken.Type = MarkdownLineTokenType.Head;
+                var matches = HeadRegex.Match(line).Groups;
+                if (matches.Count > 2)
+                {
+                    tokensList.Add(new MarkdownToken() { Index = matches[1].Index + matches[1].Length - 1, Type = MarkdownTokenType.Head });
+                    tokensList.AddRange(GetTextTokens(matches[2].Value));
+                }
+            }
+            else if (ListItemRegex.IsMatch(line))
+            {
+                lineToken.Type = MarkdownLineTokenType.List;
+                var matches = ListItemRegex.Match(line).Groups;
+                tokensList.Add(new MarkdownToken() { Index = matches[1].Index + matches[1].Length - 1, Type = MarkdownTokenType.ListItem });
+                if (matches.Count > 2)
+                {
+                    tokensList.AddRange(GetTextTokens(matches[2].Value));
+                }
+            }
+            else if (CodeBlockRegex.IsMatch(line))
+            {
+                lineToken.Type = MarkdownLineTokenType.CodeBlock;
+            }
+            else
+            {
+                lineToken.Type = MarkdownLineTokenType.Paragraph;
+                tokensList.AddRange(GetTextTokens(line));
+            }
+            lineToken.Tokens = tokensList.ToArray();
+            return lineToken;
+        }
         private static IEnumerable<MarkdownLineToken> GetTokens(List<string> lines)
         {
-            MarkdownLineTokenType blockType = MarkdownLineTokenType.None;
             List<MarkdownToken> tokensList = new List<MarkdownToken>();
             for (int i = 0; i < lines.Count; i++)
             {
                 string line = lines[i];
                 MarkdownLineToken lineToken = new MarkdownLineToken() { Line = i };
                 tokensList.Clear();
-                if (blockType == MarkdownLineTokenType.CodeBlock)
+                if (line.Length == 0)
                 {
-                    if (line.StartsWith("```"))
+                    lineToken.Type = MarkdownLineTokenType.None;
+                }
+                else if (HeadRegex.IsMatch(line))
+                {
+                    lineToken.Type = MarkdownLineTokenType.Head;
+                    var matches = HeadRegex.Match(line).Groups;
+                    if (matches.Count > 2)
                     {
-                        blockType = MarkdownLineTokenType.None;
-                        lineToken.Type = MarkdownLineTokenType.CodeBlock;
+                        tokensList.Add(new MarkdownToken() { Index = matches[1].Index + matches[1].Length - 1, Type = MarkdownTokenType.Head });
+                        tokensList.AddRange(GetTextTokens(matches[2].Value));
                     }
-                    else
+                }
+                else if (ListItemRegex.IsMatch(line))
+                {
+                    lineToken.Type = MarkdownLineTokenType.List;
+                    var matches = ListItemRegex.Match(line).Groups;
+                    tokensList.Add(new MarkdownToken() { Index = matches[1].Index + matches[1].Length - 1, Type = MarkdownTokenType.ListItem });
+                    if (matches.Count > 2)
                     {
-                        blockType = MarkdownLineTokenType.CodeBlock;
-                        lineToken.Type = MarkdownLineTokenType.CodeBlock;
-                        tokensList.Add(new MarkdownToken() { Index = line.Length - 1, Type = MarkdownTokenType.Code });
+                        tokensList.AddRange(GetTextTokens(matches[2].Value));
                     }
+                }
+                else if (CodeBlockRegex.IsMatch(line))
+                {
+                    lineToken.Type = MarkdownLineTokenType.CodeBlock;
                 }
                 else
                 {
-                    line = line.Trim();
-#if NETCOREAPP2_1
-                    if (line.StartsWith('#'))
-#else
-                    if (line.StartsWith("#"))
-#endif
-                    {
-                        blockType = MarkdownLineTokenType.Head;
-                        lineToken.Type = MarkdownLineTokenType.Head;
-                        int j = 0;
-                        for (; j < line.Length; j++)
-                        {
-                            if (line[j] != '#')
-                                break;
-                        }
-                        tokensList.Add(new MarkdownToken() { Index = j - 1, Type = MarkdownTokenType.Head });
-                        int k = line.Length - 1;
-                        for (; k >= 0; k--)
-                        {
-                            if (line[k] != '#')
-                                break;
-                        }
-                        tokensList.AddRange(GetTextTokens(line.Substring(j, k - j + 1)));
-                    }
-                    else if (line.StartsWith("* "))
-                    {
-                        blockType = MarkdownLineTokenType.List;
-                        lineToken.Type = MarkdownLineTokenType.List;
-                        tokensList.Add(new MarkdownToken() { Index = 1, Type = MarkdownTokenType.ListItem });
-                        tokensList.AddRange(GetTextTokens(line.Substring(2)));
-                    }
-                    else if (line.StartsWith("```"))
-                    {
-                        blockType = MarkdownLineTokenType.CodeBlock;
-                        lineToken.Type = MarkdownLineTokenType.CodeBlock;
-                    }
-                    else if (line.Length == 0)
-                    {
-                        blockType = MarkdownLineTokenType.Paragraph;
-                        lineToken.Type = MarkdownLineTokenType.None;
-                    }
-                    else
-                    {
-                        blockType = MarkdownLineTokenType.Paragraph;
-                        lineToken.Type = MarkdownLineTokenType.Paragraph;
-                        tokensList.AddRange(GetTextTokens(line));
-                    }
+                    lineToken.Type = MarkdownLineTokenType.Paragraph;
+                    tokensList.AddRange(GetTextTokens(line));
                 }
                 lineToken.Tokens = tokensList.ToArray();
                 yield return lineToken;
             }
         }
 
+        private static readonly Regex InlineCodeRegex = new Regex(@"[ ]*([\`])([^`]+)([\`])");
         private static IEnumerable<MarkdownToken> GetTextTokens(string text)
         {
-            for (int i = 0; i < text.Length; i++)
+            var matches = InlineCodeRegex.Matches(text);
+            foreach (Match match in matches)
             {
-                if (text[i] == '`')
+                if (match.Index > 1)
                 {
-                    if (i > 1)
-                    {
-                        yield return new MarkdownToken() { Index = i - 1, Type = MarkdownTokenType.Text };
-                    }
-                    yield return new MarkdownToken() { Index = i, Type = MarkdownTokenType.Code };
+                    yield return new MarkdownToken() { Index = match.Index - 1, Type = MarkdownTokenType.Text };
+                }
+                yield return new MarkdownToken() { Index = match.Groups[1].Index, Type = MarkdownTokenType.None };
+                yield return new MarkdownToken() { Index = match.Groups[2].Index + match.Groups[2].Length - 1, Type = MarkdownTokenType.Code };
+                yield return new MarkdownToken() { Index = match.Groups[3].Index, Type = MarkdownTokenType.None };
+            }
+            if (matches.Count > 0)
+            {
+                Match last = matches[matches.Count - 1];
+                if (last.Index + last.Length >= text.Length)
+                {
+                    yield break;
                 }
             }
-            if (text.Length > 0 && text[text.Length - 1] != '`')
-            {
-                yield return new MarkdownToken() { Index = text.Length - 1, Type = MarkdownTokenType.Text };
-            }
-        }
-
-        private static IEnumerable<string> ReadLines(StreamReader reader)
-        {
-            while (!reader.EndOfStream)
-            {
-                yield return reader.ReadLine();
-            }
+            yield return new MarkdownToken() { Index = text.Length - 1, Type = MarkdownTokenType.Text };
         }
     }
 }
